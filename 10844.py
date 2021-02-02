4b338658882467b0216c9b51f840ba7571e34d08
ldldz <qkrqudrjs4350@gmail.com>
2021-02-02 21:04:34 +0900

    Correct 10844
    백준 10844 쉬운 계단 수 정답코드

---
+++ 10844.py
@@ -0,0 +1,10 @@
+n = int(input())
+stair = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+tmp = [0] * 10
+for _ in range(n - 1):
+    tmp[0] = stair[1]
+    tmp[9] = stair[8]
+    for i in range(1, 9):
+        tmp[i] = stair[i - 1] + stair[i + 1]
+    stair = tmp.copy()
+print(sum(stair) % 1000000000)
